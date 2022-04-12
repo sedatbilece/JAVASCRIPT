57d82ba8cb5978d5c95aca4c772cec6047c4d989
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-04-12 15:49:09 +0300

    class basics

---
+++ class/basic-syntax.js
@@ -0,0 +1,25 @@
+class User {
+
+    constructor(name) {// new ile User çağrıldığında çağrılır
+      // invokes the setter
+      this.name = name;
+    }
+  
+    get name() {//name değişkeninin get methodu
+      return this._name;// _name get methodu name ile karışmaması için
+    }
+  
+    set name(value) {//name değişkeninin set methodu
+      if (value.length < 4) {
+        console.log("Name is too short.");
+        return;
+      }
+      this._name = value;
+    }
+  
+  }
+  
+  let user = new User("John");
+  console.log(user.name); // John
+  
+  user = new User(""); // Name is too short.
