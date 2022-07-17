5cab43da375378268469b6978445b25a11290fa4
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-07-17 14:16:03 +0300

    AsyncAndAwait

---
+++ async/asynsample/AsyncAndAwait.js
@@ -0,0 +1,14 @@
+
+
+( async()=>{
+    async function hello(){
+
+        //  return new Promise(resolve => resolve("hello world"));
+        return "async hello world";
+      }
+      
+      const text =await hello();
+      //text.then( response =>  console.log(response));
+      console.log(text);
+
+})()
