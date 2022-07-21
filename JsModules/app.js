d6c3a452fa3d581df2bbc508107c159a42e8e0ed
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-07-21 21:44:24 +0300

    Module Structure

---
+++ JsModules/app.js
@@ -0,0 +1,5 @@
+import {hello,sayHi} from "./utils.js";
+
+
+console.log(hello());
+console.log(sayHi('John'));
---
+++ JsModules/utils.js
@@ -0,0 +1,16 @@
+export function hello() {
+  return 'Hello world!';
+}
+
+ let sayHi = (name) => `Hi ${name}`;
+
+ let API_URL = 'http://localhost:3000';
+
+//export default hello;
+
+
+export {
+    sayHi,
+    API_URL
+    
+    };
