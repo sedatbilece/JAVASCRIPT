7f7b508ce8f1f4946a8611af85d643470ddb26bb
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-08-11 11:50:54 +0300

    CodeWars Opposite Numbers

---
+++ CodeWars/Opposite-number.js
@@ -0,0 +1,14 @@
+const opposite = number =>  0 - number ;
+
+
+ console.log( opposite(1.25))
+
+ /*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+ */
